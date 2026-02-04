af54193394069f50d4ca8218c8d4ad0e54ec5d54
maikdevries <dev.maik.devries@outlook.com>
2026-02-04 11:01:16 +0100

    feat: define Node and Graph structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+struct Node {
+    neighbours: Vec<usize>,
+}
+
+impl Node {
+    fn new(neighbours: Vec<usize>) -> Self {
+        Self { neighbours }
+    }
+}
+
+struct Graph {
+    nodes: Vec<Node>,
+}
+
+impl Graph {
+    fn new(adjacency: Vec<Vec<usize>>) -> Self {
+        Self {
+            nodes: adjacency.into_iter().map(Node::new).collect(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let graph = Graph::new(vec![vec![1], vec![2], vec![3], vec![4], vec![0]]);
 }
