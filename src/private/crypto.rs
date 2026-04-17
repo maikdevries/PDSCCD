757eaa09f905eaf920172ad45939eb3bf0f08968
maikdevries <dev.maik.devries@outlook.com>
2026-04-17 13:14:00 +0200

    refactor: merge elliptic and threshold crypto modules

--- src/private/crypto.rs
+++ src/private/crypto.rs
@@ -1,2 +1,256 @@
-mod elliptic;
-mod threshold;
+use curve25519_dalek::{RistrettoPoint, Scalar, constants::RISTRETTO_BASEPOINT_POINT as G};
+
+#[derive(Clone, Copy, Default, Eq, PartialEq)]
+pub struct Ciphertext {
+    randomness: RistrettoPoint,
+    message: RistrettoPoint,
+}
+
+#[derive(Clone, Copy, Eq, PartialEq)]
+pub struct Plaintext {
+    point: RistrettoPoint,
+}
+
+impl From<u128> for Plaintext {
+    fn from(value: u128) -> Self {
+        Self {
+            point: RistrettoPoint::hash_from_bytes::<sha3::Sha3_512>(&value.to_ne_bytes()),
+        }
+    }
+}
+
+pub struct Partial {
+    index: usize,
+    value: RistrettoPoint,
+}
+
+impl Partial {
+    fn lagrange(indices: &[usize], index: usize) -> Scalar {
+        let x = Scalar::from(index as u128);
+
+        let mut numerator = Scalar::ONE;
+        let mut denominator = Scalar::ONE;
+
+        for &i in indices {
+            if i == index {
+                continue;
+            }
+
+            let y = Scalar::from(i as u128);
+            numerator *= -y;
+            denominator *= x - y;
+        }
+
+        return numerator * denominator.invert();
+    }
+}
+
+struct Share {
+    index: usize,
+    value: Scalar,
+}
+
+impl Share {
+    fn new(coefficients: &[Scalar], index: usize) -> Self {
+        let x = Scalar::from(index as u128);
+
+        let mut result = Scalar::ZERO;
+        let mut power = Scalar::ONE;
+
+        for coefficient in coefficients {
+            result += coefficient * power;
+            power *= x;
+        }
+
+        Share {
+            index: index,
+            value: result,
+        }
+    }
+}
+
+pub struct Threshold {
+    private: Share,
+    public: RistrettoPoint,
+}
+
+impl Threshold {
+    pub fn setup<const T: usize, const N: usize>() -> [Self; N] {
+        // [NOTE]
+        let coefficients = [0; T].map(|_| Scalar::random(&mut rand::rng()));
+        let public = coefficients[0] * G;
+
+        // [NOTE]
+        return std::array::from_fn(|i| Self {
+            public: public,
+            private: Share::new(&coefficients, i + 1),
+        });
+    }
+
+    pub fn encrypt(&self, message: &Plaintext) -> Ciphertext {
+        let r = Scalar::random(&mut rand::rng());
+
+        Ciphertext {
+            randomness: r * G,
+            message: message.point + r * self.public,
+        }
+    }
+
+    pub fn rerandomise(&self, cipher: &Ciphertext) -> Ciphertext {
+        let r = Scalar::random(&mut rand::rng());
+
+        Ciphertext {
+            randomness: cipher.randomness + r * G,
+            message: cipher.message + r * self.public,
+        }
+    }
+
+    pub fn decrypt(&self, cipher: &Ciphertext) -> Partial {
+        Partial {
+            index: self.private.index,
+            value: self.private.value * cipher.randomness,
+        }
+    }
+
+    pub fn combine(partials: &[Partial], cipher: Ciphertext) -> Plaintext {
+        let indices: Vec<usize> = partials.iter().map(|partial| partial.index).collect();
+        let mut secret = RistrettoPoint::default();
+
+        for partial in partials {
+            let lambda = Partial::lagrange(&indices, partial.index);
+            secret += lambda * partial.value;
+        }
+
+        Plaintext {
+            point: cipher.message - secret,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::seq::IndexedRandom;
+
+    use super::*;
+
+    #[test]
+    fn encode_onto_curve_deterministic() {
+        let value = rand::random::<u128>();
+
+        let a = Plaintext::from(value);
+        let b = Plaintext::from(value);
+
+        assert_eq!(a, b);
+    }
+
+    #[test]
+    fn encrypt_probabilistic() {
+        let participants = Threshold::setup::<3, 8>();
+
+        let value = rand::random::<u128>();
+        let message = Plaintext::from(value);
+
+        let a = participants
+            .choose(&mut rand::rng())
+            .expect("Participants should not be empty")
+            .encrypt(&message);
+
+        let b = participants
+            .choose(&mut rand::rng())
+            .expect("Participants should not be empty")
+            .encrypt(&message);
+
+        assert_ne!(a, b);
+    }
+
+    #[test]
+    fn combine() {
+        let participants = Threshold::setup::<3, 8>();
+
+        let value = rand::random::<u128>();
+        let message = Plaintext::from(value);
+
+        let cipher = participants
+            .choose(&mut rand::rng())
+            .expect("Participants should not be empty")
+            .encrypt(&message);
+
+        let partials: Vec<Partial> = participants
+            .sample(&mut rand::rng(), 3)
+            .map(|participant| participant.decrypt(&cipher))
+            .collect();
+
+        let plain = Threshold::combine(&partials, cipher);
+
+        assert_eq!(plain, message);
+    }
+
+    #[test]
+    fn combine_insufficient() {
+        let participants = Threshold::setup::<3, 8>();
+
+        let value = rand::random::<u128>();
+        let message = Plaintext::from(value);
+
+        let cipher = participants
+            .choose(&mut rand::rng())
+            .expect("Participants should not be empty")
+            .encrypt(&message);
+
+        let partials: Vec<Partial> = participants
+            .sample(&mut rand::rng(), rand::random_range(1..3))
+            .map(|participant| participant.decrypt(&cipher))
+            .collect();
+
+        let plain = Threshold::combine(&partials, cipher);
+
+        assert_ne!(plain, message);
+    }
+
+    #[test]
+    fn rerandomise_indistinguishable() {
+        let participants = Threshold::setup::<3, 8>();
+
+        let value = rand::random::<u128>();
+        let message = Plaintext::from(value);
+
+        let a = participants
+            .choose(&mut rand::rng())
+            .expect("Participants should not be empty")
+            .encrypt(&message);
+
+        let b = participants
+            .choose(&mut rand::rng())
+            .expect("Participants should not be empty")
+            .rerandomise(&a);
+
+        assert_ne!(a, b);
+    }
+
+    #[test]
+    fn rerandomise_preserving() {
+        let participants = Threshold::setup::<3, 8>();
+
+        let value = rand::random::<u128>();
+        let message = Plaintext::from(value);
+
+        let a = participants
+            .choose(&mut rand::rng())
+            .expect("Participants should not be empty")
+            .encrypt(&message);
+
+        let b = participants
+            .choose(&mut rand::rng())
+            .expect("Participants should not be empty")
+            .rerandomise(&a);
+
+        let partials: Vec<Partial> = participants
+            .sample(&mut rand::rng(), 3)
+            .map(|participant| participant.decrypt(&b))
+            .collect();
+
+        let plain = Threshold::combine(&partials, b);
+
+        assert_eq!(plain, message);
+    }
+}
