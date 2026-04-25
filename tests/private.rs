1acac007a433510921ec63079b57c762f52099f7
maikdevries <dev.maik.devries@outlook.com>
2026-04-25 18:58:40 +0200

    test: update tests to latest changes

--- tests/private.rs
+++ tests/private.rs
@@ -1,14 +1,18 @@
 use pcd::private::{
-    core::{Graph, Location, Node},
+    core::{Graph, Location, Node, Participant},
+    crypto::STTP,
     protocol::Protocol,
 };
 
 #[test]
 fn success() {
+    let sttp = STTP::new();
+
     let participants = [
-        (
+        Participant::new(
             "A",
             6,
+            &sttp,
             Graph::new([
                 Node::new(0, Location::External("C"), [1]),
                 Node::new(1, Location::Internal, [2]),
@@ -16,9 +20,10 @@ fn success() {
                 Node::new(3, Location::External("B"), []),
             ]),
         ),
-        (
+        Participant::new(
             "B",
             6,
+            &sttp,
             Graph::new([
                 Node::new(2, Location::External("A"), [3]),
                 Node::new(3, Location::Internal, [4]),
@@ -26,9 +31,10 @@ fn success() {
                 Node::new(5, Location::External("C"), []),
             ]),
         ),
-        (
+        Participant::new(
             "C",
             6,
+            &sttp,
             Graph::new([
                 Node::new(4, Location::External("B"), [5]),
                 Node::new(5, Location::Internal, [0]),
@@ -48,26 +54,31 @@ fn success() {
 
 #[test]
 fn no_external_nodes() {
+    let sttp = STTP::new();
+
     let participants = [
-        (
+        Participant::new(
             "A",
             6,
+            &sttp,
             Graph::new([
                 Node::new(1, Location::Internal, [2]),
                 Node::new(2, Location::Internal, []),
             ]),
         ),
-        (
+        Participant::new(
             "B",
             6,
+            &sttp,
             Graph::new([
                 Node::new(3, Location::Internal, [4]),
                 Node::new(4, Location::Internal, []),
             ]),
         ),
-        (
+        Participant::new(
             "C",
             6,
+            &sttp,
             Graph::new([
                 Node::new(5, Location::Internal, [0]),
                 Node::new(0, Location::Internal, []),
@@ -86,28 +97,33 @@ fn no_external_nodes() {
 
 #[test]
 fn no_external_incoming() {
+    let sttp = STTP::new();
+
     let participants = [
-        (
+        Participant::new(
             "A",
             6,
+            &sttp,
             Graph::new([
                 Node::new(1, Location::Internal, [2]),
                 Node::new(2, Location::Internal, [3]),
                 Node::new(3, Location::External("B"), []),
             ]),
         ),
-        (
+        Participant::new(
             "B",
             6,
+            &sttp,
             Graph::new([
                 Node::new(3, Location::Internal, [4]),
                 Node::new(4, Location::Internal, [5]),
                 Node::new(5, Location::External("C"), []),
             ]),
         ),
-        (
+        Participant::new(
             "C",
             6,
+            &sttp,
             Graph::new([
                 Node::new(5, Location::Internal, [0]),
                 Node::new(0, Location::Internal, [1]),
@@ -127,28 +143,33 @@ fn no_external_incoming() {
 
 #[test]
 fn no_external_outgoing() {
+    let sttp = STTP::new();
+
     let participants = [
-        (
+        Participant::new(
             "A",
             6,
+            &sttp,
             Graph::new([
                 Node::new(0, Location::External("C"), [1]),
                 Node::new(1, Location::Internal, [2]),
                 Node::new(2, Location::Internal, []),
             ]),
         ),
-        (
+        Participant::new(
             "B",
             6,
+            &sttp,
             Graph::new([
                 Node::new(2, Location::External("A"), [3]),
                 Node::new(3, Location::Internal, [4]),
                 Node::new(4, Location::Internal, []),
             ]),
         ),
-        (
+        Participant::new(
             "C",
             6,
+            &sttp,
             Graph::new([
                 Node::new(4, Location::External("B"), [5]),
                 Node::new(5, Location::Internal, [0]),
@@ -168,10 +189,13 @@ fn no_external_outgoing() {
 
 #[test]
 fn component_size_exceeded() {
+    let sttp = STTP::new();
+
     let participants = [
-        (
+        Participant::new(
             "A",
             5,
+            &sttp,
             Graph::new([
                 Node::new(0, Location::External("C"), [1]),
                 Node::new(1, Location::Internal, [2]),
@@ -179,9 +203,10 @@ fn component_size_exceeded() {
                 Node::new(3, Location::External("B"), []),
             ]),
         ),
-        (
+        Participant::new(
             "B",
             5,
+            &sttp,
             Graph::new([
                 Node::new(2, Location::External("A"), [3]),
                 Node::new(3, Location::Internal, [4]),
@@ -189,9 +214,10 @@ fn component_size_exceeded() {
                 Node::new(5, Location::External("C"), []),
             ]),
         ),
-        (
+        Participant::new(
             "C",
             5,
+            &sttp,
             Graph::new([
                 Node::new(4, Location::External("B"), [5]),
                 Node::new(5, Location::Internal, [0]),
@@ -212,10 +238,13 @@ fn component_size_exceeded() {
 
 #[test]
 fn participant_specific_component_size() {
+    let sttp = STTP::new();
+
     let participants = [
-        (
+        Participant::new(
             "A",
             5,
+            &sttp,
             Graph::new([
                 Node::new(0, Location::External("C"), [1]),
                 Node::new(1, Location::Internal, [2]),
@@ -223,9 +252,10 @@ fn participant_specific_component_size() {
                 Node::new(3, Location::External("B"), []),
             ]),
         ),
-        (
+        Participant::new(
             "B",
             6,
+            &sttp,
             Graph::new([
                 Node::new(2, Location::External("A"), [3]),
                 Node::new(3, Location::Internal, [4]),
@@ -233,9 +263,10 @@ fn participant_specific_component_size() {
                 Node::new(5, Location::External("C"), []),
             ]),
         ),
-        (
+        Participant::new(
             "C",
             7,
+            &sttp,
             Graph::new([
                 Node::new(4, Location::External("B"), [5]),
                 Node::new(5, Location::Internal, [0]),
