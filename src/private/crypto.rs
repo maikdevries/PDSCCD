1ed000d6e3cc39628cf2cc23c0ff02b669b8384a
maikdevries <dev.maik.devries@outlook.com>
2026-04-29 16:23:39 +0200

    feat: add paillier to crypto suite

--- src/private/crypto.rs
+++ src/private/crypto.rs
@@ -1,16 +1,19 @@
-use crate::private::crypto::elliptic::Elliptic;
+use crate::private::crypto::{elliptic::*, paillier::*};
 
 pub mod elliptic;
+pub mod paillier;
 mod threshold;
 
 pub struct Crypto {
     pub elliptic: Elliptic,
+    pub paillier: Paillier,
 }
 
 impl Crypto {
     pub fn new() -> Self {
         Self {
             elliptic: Elliptic::new(),
+            paillier: Paillier::new(),
         }
     }
 }
