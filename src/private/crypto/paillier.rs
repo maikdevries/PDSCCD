a8c13ad08b2436578beb7f347a9e69aa98c9b24a
maikdevries <dev.maik.devries@outlook.com>
2026-04-30 09:39:14 +0200

    refactor: encryption is re-randomisation of fresh cipher

--- src/private/crypto/paillier.rs
+++ src/private/crypto/paillier.rs
@@ -56,24 +56,7 @@ impl Paillier {
             .add_mod(&U4096::ONE, &self.public.squared.to_nz_vartime().unwrap());
 
         // [NOTE]
-        let r = U2048::random_mod_vartime(
-            &mut rand::rng(),
-            &self.public.modulus.to_nz_vartime().unwrap(),
-        );
-
-        // [TODO] Greatest common divisor of r and modulus must be 1
-        assert_ne!(r, U2048::ZERO, "Randomness must not be zero");
-
-        // [NOTE]
-        let randomness = FixedMontyForm::new(
-            &r.resize(),
-            &FixedMontyParams::new_vartime(self.public.squared.to_odd().unwrap()),
-        )
-        .pow_vartime(&self.public.modulus)
-        .retrieve();
-
-        // [NOTE]
-        return cipher.mul_mod_vartime(&randomness, &self.public.squared.to_nz_vartime().unwrap());
+        return self.rerandomise(&cipher);
     }
 
     pub fn decrypt(&self, cipher: &Ciphertext) -> Plaintext {
